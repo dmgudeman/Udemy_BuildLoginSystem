d02210ce4225b90441ffaaf8687f644ae6ac4add
davidmgudeman <davidmgudeman@gmail.com>
2017-05-18 14:46:09 -0700

    finished the encryption of password with bcryptjs

--- Project01/nodeauth/models/user.js
+++ Project01/nodeauth/models/user.js
@@ -1,5 +1,6 @@
 
 var mongoose = require('mongoose');
+let bcrypt = require('bcryptjs');
 
 mongoose.connect('mongodb://localhost/nodeauth');
 
@@ -28,5 +29,10 @@ let UserSchema = mongoose.Schema({
 let User = module.exports = mongoose.model('User', UserSchema);
 
 module.exports.createUser = function(newUser, callback) {
+    bcrypt.genSalt(10, function(err, salt) {
+    bcrypt.hash("newUser.password", salt, function(err, hash) {
+    newUser.password = hash;
     newUser.save(callback);
+    });
+});
 }
